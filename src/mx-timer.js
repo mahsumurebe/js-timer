f84dfbe91c8bfef6be211a88364d0e89d8784cbf
Mahsum UREBE <mahsumurebe@outlook.com>
2017-09-01 04:11:12 +0300

    Upgrade and Fixed Bugs
    Added new methods and fixed bugs.

--- src/mx-timer.js
+++ src/mx-timer.js
@@ -1,19 +1,46 @@
+/**
+ * MXTimer - Timer Plugin for JavaScript
+ *
+ * The purpose of the plugin is to collect, manage and group
+ * the timers used in a single class. The plugin is free to use.
+ * Distribution and development is only open if it is to be
+ * distributed free of charge.
+ *
+ * For donation: http://www.fb.com/mahsumurebe :)
+ *
+ * @author Mahsum UREBE | https://github.com/mahsumurebe/
+ * @since 2017
+ */
 'use strict'
+/**
+ * Argument error catcher.
+ * @param {string} message
+ * @constructor
+ */
 function TimerInvalidAddArgs(message) {
     this.message = message + ' For details: http://www.github.com/mahsumurebe/js-timer/#errors';
     this.name = 'TimerInvalidAddArgument';
 }
+/**
+ * The error trapping function called because the correct function was not
+ * @param {string} message
+ * @constructor
+ */
 function TimerIsNotFunction(message) {
     this.message = message + ' For details: http://www.github.com/mahsumurebe/js-timer/#errors';
     this.name = 'TimerIsNotFunction';
 }
+/**
+ * MXTimer class
+ *
+ * @type {{add, stop, getByGroupName, info, remove}}
+ */
 var MXTimer = function () {
     /**
      * Timer in groups
      * @type {{}}
      */
     var timers = {};
-
     /**
      * Get more realistic types
      * @param {*} arg
@@ -34,11 +61,6 @@ var MXTimer = function () {
     var is_function = function (arg) {
         return get_type(arg) === 'function';
     };
-    String.prototype.capitalize = function () {
-        return this.replace(/(?:^|\s)\S/g, function (a) {
-            return a.toUpperCase();
-        });
-    };
     /**
      * Check is object
      * @param {*} arg argument
@@ -55,7 +77,6 @@ var MXTimer = function () {
     var is_array = function (arg) {
         return get_type(arg) === "array";
     };
-
     /**
      * Check is undefined
      * @param {*} arg argument
@@ -64,16 +85,49 @@ var MXTimer = function () {
     var is_undefined = function (arg) {
         return get_type(arg) === "undefined";
     };
+    // If {}.merge not defined, define object merge
+    if (is_undefined(Object.prototype.merge)) {
+        /**
+         * Define object merge.
+         *
+         * @returns {{}}
+         */
+        Object.prototype.merge = function () {
+            'use strict';
+            var dst = {};
+            var args = [].splice.call(arguments, 0);
+            var item;
+            while (args.length > 0) {
+                item = args.splice(0, 1)[0];
+                if (is_object(item)) {
+                    for (var q in item) {
+                        if (item.hasOwnProperty(q)) {
+                            if (is_object(item[q]))
+                                dst[q] = {}.merge(dst[q] || {}, item[q]);
+                            else
+                                dst[q] = item[q];
+                        }
+                    }
+                }
+            }
+            return dst;
+        }
+    }
     /**
      * Add timer
-     * @param {object} opts
-     * @returns {{onafterStart:onafterstart,onstart:onstart,onafterStop:onafterstop,onstop:onstop,callback:callback,getCount:getCount, trigger: _trigger, count: number, handle: null, start: start, reset:reset, stop: stop, _initializer: _initializer}}
+     *
+     * @param {object} opts Options of timer.
+     * @returns {{count: number, handle: null, run_times: Array, getCount: getCount, isStarted: isStarted, _trigger: _trigger, trigger: trigger, on: on, start: start, reset: reset, stop: stop, _initializer: _initializer}}
      */
     var add = function (opts) {
         if (!is_object(opts))
             throw new TimerInvalidAddArgs('Invalid argument was sent for adding timer.');
-        // options
-        var options = Object.assign({
+        /**
+         * Options object.
+         *
+         * @type {Object} options
+         */
+        var options = {}.merge({
             func: function () {
 
             },
@@ -82,10 +136,10 @@ var MXTimer = function () {
             interval: 0,
             autoStart: false,
             initializer: function () {
-                },
-                onafterstart: function () {
+            },
+            onafterstart: function () {
 
-                },
+            },
             onstart: function () {
 
             },
@@ -99,7 +153,6 @@ var MXTimer = function () {
 
             }
         }, opts);
-
         if (!is_function(options.func))
             throw new TimerIsNotFunction('Timer function not defined correctly.');
         if (options.interval < 1)
@@ -109,18 +162,36 @@ var MXTimer = function () {
         }
         if (!timers.hasOwnProperty(options.group_name))
             timers[options.group_name] = [];
-
+        /**
+         * Timer object.
+         *
+         * @type {Object}
+         */
         var timer = {
             count: 0,
             handle: null,
             run_times: [],
+            status: 0,
+            /**
+             * Function that returns the number of times the timer has been run.
+             * @returns {number}
+             */
             getCount: function () {
                 return this.count;
             },
             /**
+             * Is timer started ?
+             *
+             * @returns {boolean}
+             */
+            isStarted: function () {
+                return this.status === 1;
+            },
+            /**
              * Trigger event on timer.
-             * @param {string} method Method name
-             * @param {[]} params
+             * @param {string} method Event name
+             * @param {Array} params
+             * @private
              * @returns {timer}
              */
             _trigger: function (method, params) {
@@ -142,13 +213,19 @@ var MXTimer = function () {
                     }
                 return this;
             },
+            /**
+             * Trigger event on timer.
+             * @param {string} method Event Name
+             * @param {Array} params Send params to event
+             * @returns {timer}
+             */
             trigger: function (method, params) {
-                return this.trigger('on' + method, 'params');
+                return this._trigger('on' + method, params);
             },
             /**
              * Set event
-             * @param {string} method
-             * @param {function} func
+             * @param {string} method Event Name
+             * @param {function} func Event Function
              * @returns {timer}
              */
             on: function (method, func) {
@@ -172,14 +249,16 @@ var MXTimer = function () {
              * @returns {timer}
              */
             start: function () {
-                this.handle = setInterval(function ($this) {
-                    $this._trigger('onstart', [this]);
-                    $this.run_times.push(new Date());
-                    var ret = $this.func.apply($this, $this.args);
-                    $this.count++;
-                    $this._trigger('callback', [$this, ret]);
-                    $this._trigger('onafterstart', [$this, ret]);
-                }, this.interval, this);
+                if (!this.handle)
+                    this.handle = setInterval(function ($this) {
+                        $this.run_times.push(new Date());
+                        $this._trigger('onstart', [this]);
+                        $this.status = 1; // set status to started
+                        var ret = $this.func.apply($this, $this.args);
+                        $this.count++;
+                        $this._trigger('callback', [$this, ret]);
+                        $this._trigger('onafterstart', [$this, ret]);
+                    }, this.interval, this);
 
                 return this;
             },
@@ -187,7 +266,7 @@ var MXTimer = function () {
              * Reset timer.
              *
              * @see timer.stop(true).start();
-             * @returns {*|number}
+             * @returns {timer}
              */
             reset: function () {
                 return this.stop(true).start();
@@ -199,10 +278,13 @@ var MXTimer = function () {
              */
             stop: function (clear) {
                 this._trigger('onstop', [this]);
-                if (clear)
+                if (clear) {
                     this.count = 0;
+                    this.run_times = [];
+                }
                 if (this.handle !== null) {
                     clearInterval(this.handle);
+                    this.status = 0; // set status stopped.
                     this.handle = null;
                     this._trigger('onafterstop')
                 }
@@ -215,10 +297,10 @@ var MXTimer = function () {
             _initializer: function () {
                 if (this.autoStart === true)
                     this.start();
-                this._trigger('initializer');
+                this._trigger('initializer', []);
             }
         };
-        Object.assign(timer, options);
+        timer = {}.merge(timer, options);
         timers[options.group_name].push(timer);
         timer._initializer();
         return timer;
@@ -226,11 +308,11 @@ var MXTimer = function () {
     /**
      * Get timers by group name;
      * @param {string} group_name
-     * @returns {[]}
+     * @returns {Array}
      */
     var getByGroupName = function (group_name) {
         if (timers.hasOwnProperty(group_name))
-            return group_name;
+            return timers[group_name];
         return [];
     };
     /**
@@ -243,13 +325,31 @@ var MXTimer = function () {
         var items = (!is_undefined(group_name) ? [getByGroupName(group_name)] : timers);
         for (var i in items) {
             if (items.hasOwnProperty(i)) {
-                var timer = items[i];
-                timer.stop(clear);
+                for (var j in items[i]) {
+                    if (items[i].hasOwnProperty(j)) {
+                        var timer = items[i][j];
+                        timer.stop(clear);
+                    }
+                }
             }
         }
         return this;
     };
-
+    /**
+     * Remove timer group
+     * @param {string}  group_name Group name
+     *
+     * @returns {remove}
+     */
+    var remove = function (group_name) {
+        stop(true, group_name);
+        for (var i in timers) {
+            if (timers.hasOwnProperty(i))
+                if (i === group_name || is_undefined(group_name))
+                    delete timers[i];
+        }
+        return this;
+    };
     /**
      * Get Timers info.
      * @param {string} group_name Group name
@@ -281,18 +381,19 @@ var MXTimer = function () {
         }
         return out;
     };
-
     return {
         add: add,
         stop: stop,
         getByGroupName: getByGroupName,
-        info: info
+        info: info,
+        remove: remove
     };
 }();
 /**
+ *
  * Timer class
- * @param {object} opts
- * @returns {{onafterStart: onafterstart, onstart: onstart, onafterStop: onafterstop, onstop: onstop, callback: callback, getCount: getCount, trigger: _trigger, count: number, handle: null, start: start, reset: reset, stop: stop, _initializer: _initializer}}
+ * @param {object} opts Options
+ * @returns {{count: number, handle: null, run_times: Array, getCount: getCount, isStarted: isStarted, _trigger: _trigger, trigger: trigger, on: on, start: start, reset: reset, stop: stop, _initializer: _initializer}|{onafterStart: onafterstart, onstart: onstart, onafterStop: onafterstop, onstop: onstop, callback: callback, getCount: getCount, trigger: _trigger, count: number, handle: null, start: start, reset: reset, stop: stop, _initializer: _initializer}}
  * @constructor
  */
 var Timer = function (opts) {
